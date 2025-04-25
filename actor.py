c8422a4961286ea534e36bc437fdb993e5b22683
hzzn <1416117478@qq.com>
2025-04-25 11:42:48 +0800

    none

--- actor.py
+++ actor.py
@@ -8,9 +8,8 @@ GAMMA = 0.99
 EPS_CLIP = 0.2
 EPOCHS = 10
 BATCH_SIZE = 64
-
+print("branch zzz")
 # 状态维度：时间段 + 每个科室的病人数 + 当前出院人数  # 每个等待病人从科室i分配到科室j
-
 # ========== Actor ==========
 class Actor(nn.Module):
     def __init__(self, J, B):
